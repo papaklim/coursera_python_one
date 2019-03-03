6ef8373497479ccea5dd81bee8c2432d26241403
papaklim <papaklim@gmail.com>
2019-03-03 22:37:18 +0300

    убраны лишние данные, не принимаемые анализатором

--- solution.py
+++ solution.py
@@ -2,12 +2,12 @@ import sys
 
 filename = sys.argv[0]
 digit_string = sys.argv[1]
-print("Имя запущенного файла: " + filename)
+# print("Имя запущенного файла: " + filename)
 if not digit_string.isdigit():
     print("Argument is not digit")
 else:
     sum = 0
     for i in digit_string:
         sum = sum + int(i)
-    print("Сумма чисел аргумента = " + str(sum))
+    print(sum)
 
