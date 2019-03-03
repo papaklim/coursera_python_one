313a11e416ab33f099c4f30ad1d794523584201b
papaklim <papaklim@gmail.com>
2019-03-03 12:12:57 +0300

    add random guess number + checking digit input

--- guessnumber.py
+++ guessnumber.py
@@ -1,15 +1,22 @@
+import random
+_test = 21
 print("""
 Угадай загаданное число!!!
 --------------------------
-Для выхода из программы введите \"Exit\"
+Для выхода из программы введите \"Exit\" или нажмите Enter с пустым значением.
 """)
-win_number = 99
+win_number = random.randint(0,1000)
 while True:
     input_number = (input("Введите число: "))
     if input_number == "Exit" or not input_number:
-        print("Вы вышли из программы")
+        print("*Вы вышли из программы*")
         break
-    elif int(input_number) > win_number:
+
+    if not input_number.isdigit():
+        print("Введите корректное значение")
+        continue
+
+    if int(input_number) > win_number:
         print("Загаданное число меньше")
     elif int(input_number) < win_number:
         print("Загаданное число больше")
