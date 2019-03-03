06e64af22a8b8191932c026d952361eafe4762c7
papaklim <papaklim@gmail.com>
2019-03-03 22:37:51 +0300

    убраны лишние данные, не принимаемые анализатором

--- sqrt.py
+++ sqrt.py
@@ -5,13 +5,17 @@ c = int(sys.argv[3])
 
 d = int(b) ** 2 - 4 * int(a) * int(c)
 
-print("D = " + str(d))
+# print("D = " + str(d))
 if d < 0:
     print("Уравнение не имеет квадратных корней")
 elif d == 0:  # например a = 1, b = 6, c = 9
-    x = int(-int(b)/2*int(a))
-    print("Уравнение имеет 1 квадратный корень x = " + str(x))
+    x1= x2 = int(-int(b)/2*int(a))
+    # print("Уравнение имеет 1 квадратный корень x = " + str(x))
+    print(x1)
+    print(x2)
 else:
-    x1 = int((-int(b) + d ** 0.5)/2)
-    x2 = int((-int(b) - d ** 0.5)/2)
-    print("Уравнение имеет 2 квадратных корня x1 = " + str(x1) + "; x2 = " + str(x2))
+    x1 = int((-b + d ** 0.5)/2 * a)
+    x2 = int((-b - d ** 0.5)/2 * a)
+    # print("Уравнение имеет 2 квадратных корня x1 = " + str(x1) + "; x2 = " + str(x2))
+    print(x2)
+    print(x1)
