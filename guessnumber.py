0a5a06ba92d7b323ad19f7c4948a312b9247e18e
papaklim <papaklim@gmail.com>
2019-03-03 12:16:59 +0300

    del trash

--- guessnumber.py
+++ guessnumber.py
@@ -1,11 +1,13 @@
 import random
-_test = 21
+
 print("""
 Угадай загаданное число!!!
 --------------------------
 Для выхода из программы введите \"Exit\" или нажмите Enter с пустым значением.
 """)
+
 win_number = random.randint(0,1000)
+
 while True:
     input_number = (input("Введите число: "))
     if input_number == "Exit" or not input_number:
