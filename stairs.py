6e7880b9aaf1dd85ca65b4760d12c22773faf01f
papaklim <papaklim@gmail.com>
2019-03-03 13:34:42 +0300

    add stairs.py: draw stairs from # by argument

---
+++ stairs.py
@@ -0,0 +1,20 @@
+import sys
+
+filename = sys.argv[0]
+num_steps = sys.argv[1]
+print("Имя запущенного файла: " + filename)
+if not num_steps.isdigit():
+    print("Argument is not digit")
+else:
+    space = ' '
+    step = "#"
+    count = int(num_steps)
+    space_count = count-1
+    step_count = count - space_count
+    while count > 0:
+        stairs = space*space_count + step * step_count
+        print(stairs)
+        count -= 1
+        space_count -= 1
+        step_count += 1
+
