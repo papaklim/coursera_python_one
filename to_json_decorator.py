089faea70ff290d5c93794e4a4c043243bbb8946
papaklim <papaklim@gmail.com>
2019-06-01 17:06:48 +0300

    Adding *args, **kwargs in func

--- to_json_decorator.py
+++ to_json_decorator.py
@@ -3,11 +3,12 @@ import functools
 
 def to_json(func):
     @functools.wraps(func)
-    def wrapper():
-        result = func()
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
         to_js = json.dumps(result)
         return to_js
     return wrapper
+
 
 
 @to_json
@@ -15,3 +16,4 @@ def get_data():
     return {
         'data': 42
     }
+
